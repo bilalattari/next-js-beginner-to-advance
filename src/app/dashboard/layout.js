dfb43246898be8044ccd536b46655c6fc593ef95
Bilal raza <attari1235@gmail.com>
2024-09-29 14:23:06 +0500

    Learned Layout

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <section className="min-h-screen flex">
+      <div className="w-1/4 border-r-2 border-r-purple-800">
+        <Link
+          className="p-2 text-center w-full block  hover:bg-purple-200"
+          href={"/dashboard/settings"}
+        >
+          Settings
+        </Link>
+
+        <Link
+          className="p-2 text-center w-full block  hover:bg-purple-200"
+          href={"/dashboard/profile"}
+        >
+          Profile
+        </Link>
+      </div>
+      <div className="w-3/4">{children}</div>
+    </section>
+  );
+}
---
+++ src/component/Header.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export function Header() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="bg-purple-300 flex item justify-center gap-3 items-center py-2">
+      <Link className={`link ${pathname === "/" ? "active underline" : ""} block`} href="/">
+        Home
+      </Link>
+
+      <Link
+        className={`link px-3 ${pathname === "/blogs" ? "active underline" : ""}`}
+        href="/blogs"
+      >
+        Blogs
+      </Link>
+      <Link
+        className={`link px-3 ${pathname === "/dashboard" ? "active underline" : ""}`}
+        href="/dashboard"
+      >
+        Dashboard
+      </Link>
+
+      <Link
+        className={`link px-3 ${pathname === "/contactus" ? "active underline" : ""}`}
+        href="/contactus"
+      >
+        Contact Us
+      </Link>
+
+      <Link
+        className={`link px-3 ${pathname === "/aboutus" ? "active underline" : ""}`}
+        href="/aboutus"
+      >
+        About
+      </Link>
+    </nav>
+  );
+}
